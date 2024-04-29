d7620e4c167cdc16cd89c2cc0b928a4f3fd38c2a
JulianSupercode <jscheib1305@gmail.com>
2024-04-29 15:39:30 +0200

    neue Aufgaben

---
+++ src/Aufgaben/01/1_11/basic.ts
@@ -0,0 +1,11 @@
+// console.log('Welcome :)');
+
+// // let dogName = 'Leon';
+// let dogName: number = 11;
+// // let dogName = 'elf';
+// console.log(dogName);
+
+let seventeen: number = 5;
+
+const sum = seventeen + 44
+console.log(sum);
---
+++ src/Aufgaben/01/1_8/basic.ts
@@ -0,0 +1,8 @@
+let x: number = 20
+let y: number = 30
+
+console.log(x + y);
+console.log(y-x)
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
